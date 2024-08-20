fd49a480f1ee5ce2322679bb9667ba86ae160163
aoneg83 <saharat83@gmail.com>
2024-08-20 11:50:22 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import vercel from '@sveltejs/adapter-vercel';
 import node from '@sveltejs/adapter-node';
 
-const dockerBuild = process.env.DOCKER_BUILD
+const dockerBuild = process.env.DOCKER_BUILD;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: dockerBuild ? node() : vercel({
-			runtime: '16' // or '18'
+			runtime: 'nodejs18.x' // Use 'nodejs18.x' for Vercel runtime
 		}),
 	}
 };
